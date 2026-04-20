22a789d5b2bad382d99aebb809cddfab774e7f96
pwwang <pwwang@pwwang.com>
2026-04-20 14:49:31 -0700

    1.0.5

--- pipen_annotate/__init__.py
+++ pipen_annotate/__init__.py
@@ -1,6 +1,9 @@
 from .annotate import annotate
 from .sections import Section
 
-__version__ = version = "1.0.4"
+__version__ = "1.0.5"
+
+# backward compatibility
+version = __version__
 
 __all__ = ["annotate", "Section", "version", "__version__"]
